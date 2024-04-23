49e7f75b269ad7b24a70d5de1445786769f36c0d
karunarathnawmwgms <122153208+karunarathnawmwgms@users.noreply.github.com>
2024-04-23 17:30:02 +0530

    add createsarving page

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
     return (
         <div>
             <GlobalRouter />
-            <Toaster />
+            {/* <Toaster /> */}
         </div>
     )
 }
---
+++ src/pages/dashboard/user/CreateSaving.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function () {
+  return (
+    <div className='pageTemplate2'>
+               
+               <h1 style="text-align: center;">Create New Account</h1>
+
+               
+
+
+        </div>
+  )
+}
