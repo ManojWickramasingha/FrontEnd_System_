9081af6041c0809aada33eef71c789294c10aa20
chinthakaSenarathna <chironprabath@gmail.com>
2024-04-23 17:15:35 +0530

    changin colors in Spinner component

--- src/components/general/Spinner.jsx
+++ src/components/general/Spinner.jsx
@@ -1,6 +1,6 @@
 const Spinner = () => {
     return (
-        <div className="mx-auto w-80 h-80 border-8 border-purple-300 border-t-purple-700 rounded-full animate-spin"></div>
+        <div className="mx-auto w-80 h-80 border-8 border-green-300 border-t-green-700 rounded-full animate-spin"></div>
     )
 }
 
