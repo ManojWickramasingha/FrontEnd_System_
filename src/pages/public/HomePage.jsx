5fb6f2cb8396895a2ea3f8b39688cf2df2502333
chinthakaSenarathna <chironprabath@gmail.com>
2024-04-29 21:07:58 +0530

    Now function parts is okay

--- src/pages/public/HomePage.jsx
+++ src/pages/public/HomePage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ScrollToTop from '../../components/general/ScrollToTop'
 import Button from '../../components/general/Button'
 import {BsChevronCompactLeft,BsChevronCompactRight} from 'react-icons/bs'
@@ -9,6 +9,8 @@ import analysis from '../../assets/analysis.png'
 import reporting from '../../assets/reporting.png'
 
 const HomePage = () => {
+    const [currentIndex,setCurrentIndex] = useState(0);
+
     const slides = [
         {
             url:tracking,
@@ -32,14 +34,24 @@ const HomePage = () => {
         }
     ];
 
+    const prevIndex = () => {
+        const nextIndex = currentIndex === 0 ? slides.length - 1 : currentIndex - 1;
+        setCurrentIndex(nextIndex);
+    }
+
+    const nextIndex = () => {
+        const nextIndex = currentIndex === slides.length - 1 ? 0 : currentIndex + 1;
+        setCurrentIndex(nextIndex);
+    }
+
     return (
         <div className='w-full min-h-[calc(100vh-48px)] flex flex-col justify-center items-center mx-auto'>
             <ScrollToTop />
             <div className='pageTemplate5'>
                 {/* texture */}
                 <div className='w-2/5 h-full flex flex-col justify-center items-start gap-3'>
-                    <h1 className='text-6xl font-bold'>{slides[0].main}</h1>
-                    <p className='text-2xl font-semibold'>{slides[0].sub}</p>
+                    <h1 className='text-6xl font-bold'>{slides[currentIndex].main}</h1>
+                    <p className='text-2xl font-semibold'>{slides[currentIndex].sub}</p>
                     <Button
                         label='Details'
                         onClick={() => handelClick()}
@@ -48,14 +60,14 @@ const HomePage = () => {
                     />
                 </div>
                 {/* images */}
-                <div style={{backgroundImage: `url(${slides[0].url})`}} className='w-[450px] h-[400px] bg-center bg-cover duration-500'></div>
+                <div style={{backgroundImage: `url(${slides[currentIndex].url})`}} className='w-[450px] h-[400px] bg-center bg-cover duration-500'></div>
                 {/* Left Arrow */}
                 <div className='absolute top-[50%] left-10 -translate-x-0 translate-y-[-50%] text-2xl bg-[#f5f5f5] rounded-3xl p-2 cursor-pointer'>
-                    <BsChevronCompactLeft size={30} />
+                    <BsChevronCompactLeft size={30} onClick={prevIndex} />
                 </div>
                 {/* Right Arrow */}
                 <div className='absolute top-[50%] right-10 -translate-x-0 translate-y-[-50%] text-2xl bg-[#f5f5f5] rounded-3xl p-2 cursor-pointer'>
-                    <BsChevronCompactRight size={30} />
+                    <BsChevronCompactRight size={30} onClick={nextIndex} />
                 </div>
             </div>
             <div className='pageTemplate4'>SERVICES</div>
