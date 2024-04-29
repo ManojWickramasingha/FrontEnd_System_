12c1cbcc7502ed06abf51a69d7e2a0a7b35e757c
Ashen06C <ashencharanga99@gmail.com>
2024-04-29 14:33:44 +0530

    fulfill reminder page

---
+++ src/components/ReminderComponents/ReminderSet.jsx
@@ -0,0 +1,106 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
+import IconButton from "@mui/material/IconButton";
+// import CloseIcon from '@mui/icons-material/Close';
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import { DialogContentText } from "@mui/material";
+
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  "& .MuiDialogContent-root": {
+    padding: theme.spacing(2),
+  },
+  "& .MuiDialogActions-root": {
+    padding: theme.spacing(1),
+  },
+}));
+
+export default function ReminderSet({ open, setOpen, rdate }) {
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      PaperProps={{
+        component: "form",
+        onSubmit: (event) => {
+          event.preventDefault();
+          const formData = new FormData(event.currentTarget);
+          const formJson = Object.fromEntries(formData.entries());
+          const email = formJson.email;
+          console.log("This is popup open");
+          handleClose();
+        },
+      }}
+    >
+      <DialogTitle>Set Reminder</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            label="Date"
+            value={rdate}
+            type="text"
+            fullWidth
+            disabled
+            variant="standard"
+          />
+        </DialogContentText>
+        <TextField
+          autoFocus
+          required
+          margin="dense"
+          id="name"
+          name="name"
+          label="Reminder Name"
+          type="text"
+          fullWidth
+          variant="standard"
+        />
+        <TextField
+          autoFocus
+          required
+          margin="dense"
+          id="amount"
+          name="amount"
+          label="Payment Amount"
+          type="text"
+          fullWidth
+          variant="standard"
+        />
+        <TextField
+          autoFocus
+          required
+          margin="dense"
+          id="desc"
+          name="desc"
+          label="Description"
+          type="text"
+          multiline
+          fullWidth
+          variant="standard"
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button
+          onClick={handleClose}
+          sx={{
+            
+          }}
+        >
+          Cancel
+        </Button>
+        <Button type="submit">Subscribe</Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
--- src/pages/dashboard/user/Remender.jsx
+++ src/pages/dashboard/user/Remender.jsx
@@ -1,9 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
+import MyCalendar from "../../../components/ReminderComponents/Calender";
 
 const Remender = () => {
-    return (
-        <div className='pageTemplate2'>RemenderPage</div>
-    )
-}
+  return (
+    <div className="pageTemplate2">
+      RemenderPage
+      <div className="calender">
+        <MyCalendar />
+      </div>
+    </div>
+  );
+};
 
-export default Remender
+export default Remender;
