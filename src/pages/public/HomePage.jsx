9418af5793c5a4722953559536f1c4cd97a09843
chinthakaSenarathna <chironprabath@gmail.com>
2024-05-06 19:44:12 +0530

    Services structure part is Okay

--- src/pages/public/HomePage.jsx
+++ src/pages/public/HomePage.jsx
@@ -65,7 +65,9 @@ const HomePage = () => {
                     />
                 </div>
                 {/* images */}
-                <div style={{backgroundImage: `url(${slides[currentIndex].url})`}} className='w-[475px] h-[450px] bg-center bg-cover duration-500'></div>
+                <div className='w-2/5 flex justify-center items-center'>
+                    <div style={{backgroundImage: `url(${slides[currentIndex].url})`}} className='w-[475px] h-[450px] bg-center bg-cover duration-500'></div>
+                </div>
                 {/* Left Arrow */}
                 <div className='hidden group-hover:block absolute top-[50%] left-10 -translate-x-0 translate-y-[-50%] text-2xl bg-[#f5f5f5] rounded-3xl p-2 cursor-pointer'>
                     <BsChevronCompactLeft size={30} onClick={prevIndex} />
@@ -84,7 +86,48 @@ const HomePage = () => {
                     }
                 </div>
             </div>
-            <div className='pageTemplate4'>SERVICES</div>
+            <div className='pageTemplate4'>
+                <div className='w-full flex justify-center text-5xl font-bold p-1 mb-5'>SERVICES</div>
+                <div className='h-4/5 w-full flex flex-row gap-10 justify-center items-center'>
+                    <div className='h-full w-1/4 flex flex-col justify-center items-center'>
+                        <div className='w-full h-2/4 flex justify-center items-center'>
+                            <div style={{backgroundImage: `url(${slides[0].url})`}} className='w-[200px] h-[200px] bg-center bg-cover duration-500'></div>
+                        </div>
+                        <div className='w-full h-2/4'>
+                            <h1></h1>
+                        </div>
+                    </div>
+                    <div className='h-full w-1/4 flex flex-col'>
+                        <div className='w-full h-2/4 flex justify-center items-center'>
+                            <div style={{backgroundImage: `url(${slides[3].url})`}} className='w-[200px] h-[200px] bg-center bg-cover duration-500'></div>
+                        </div>
+                        <div className='w-full h-2/4'></div>
+                    </div>
+                    <div className='h-full w-1/4 flex flex-col'>
+                        <div className='w-full h-2/4 flex justify-center items-center'>
+                            <div style={{backgroundImage: `url(${slides[2].url})`}} className='w-[200px] h-[200px] bg-center bg-cover duration-500'></div>
+                        </div>
+                        <div className='w-full h-2/4'></div>
+                    </div>
+                </div>
+                {/* Left Arrow */}
+                <div className='absolute top-[150%] left-10 -translate-x-0 translate-y-[-50%] text-2xl bg-[#fff] rounded-3xl p-2 cursor-pointer'>
+                    <BsChevronCompactLeft size={30} onClick={prevIndex} />
+                </div>
+                {/* Right Arrow */}
+                <div className='absolute top-[150%] right-10 -translate-x-0 translate-y-[-50%] text-2xl bg-[#fff] rounded-3xl p-2 cursor-pointer'>
+                    <BsChevronCompactRight size={30} onClick={nextIndex} />
+                </div>
+                <div className='flex absolute top-[185%] justify-center py-2'>
+                    {
+                        slides.map(() => (
+                            <div className='text-2xl cursor-pointer'>
+                                <RxDotFilled />
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
             <div className='pageTemplate5'>ABOUT</div>
             <div className='pageTemplate4'>CONTACT</div>
             <div className='pageTemplate5'>BOTTOM</div>
