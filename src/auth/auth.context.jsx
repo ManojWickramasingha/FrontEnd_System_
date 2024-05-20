506d67f92b450f574b143babf5ce04b7f951df68
chinthakaSenarathna <chironprabath@gmail.com>
2024-05-20 12:12:21 +0530

    A big problem occured in Authentication part

--- src/auth/auth.context.jsx
+++ src/auth/auth.context.jsx
@@ -54,7 +54,7 @@ const initialAuthState = {
 };
 
 // we create our context here and export
-export const AuthContext = createContext(null);
+export const AuthContext = createContext(null);                                                                                                                             
 
 // we create a component to manage all auth functionalities and export it and use it
 const AuthContextProvider = ({ children }) => {
@@ -62,15 +62,17 @@ const AuthContextProvider = ({ children }) => {
     const navigate = useNavigate();
 
     // Initialize Method
+    // useCallback -> avoid the unnecessary render...
     const initializeAuthContext = useCallback(async () => {
         try {
             const token = getSession();
             if(token) {
                 // validate accessToken by calling backend
                 const response = await axiosInstance.post(ME_URL, {
-                    token
+                    token,
                 });
                 // In response, we receive jwt token and user data
+                // destructure the recieve data..
                 const { newToken, userInfo } = response.data;
                 setSession(newToken);
                 dispatch({
