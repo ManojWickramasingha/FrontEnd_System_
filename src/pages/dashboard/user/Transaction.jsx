aeb78c7d32455bb1a437dec98ef886c0d7a2c33d
karunarathnawmwgms <122153208+karunarathnawmwgms@users.noreply.github.com>
2024-06-02 11:43:09 +0530

    finishfrontend

--- src/pages/dashboard/user/Transaction.jsx
+++ src/pages/dashboard/user/Transaction.jsx
@@ -47,17 +47,17 @@ const Transaction = () => {
 
     return (
         <div className='pageTemplate2'>
-            <div className='bg-gray-200 min-h-screen'>
-                <h1 className='text-6xl  text-center pt-6  text-green-500'>Expense Tracker</h1>
+            <div className='bg-white-200 min-h-screen'>
+                <h1 className='text-6xl font-bold  text-center pt-6  text-green-500'>Transaction</h1>
                 <div className='container mt-20 mt-auto px-5'>
                     <div className='p-5 bg-white rounded-lg shadow-lg'>
-                        <div>
-                        <table className='w-full table-fixed flex flex-col  items-between text-left'>
+                        <div className='flex columns-1 '>
+                         <table className='w-full table-fixed flex flex-col  items-between text-left'>
                             <thead > 
                                 <tr className=' '>
                                     <th className='text-xl font-bold w-full md:w-1/4 px-2 py-2'>Description</th>
-                                    <th className='text-xl font-bold w-full md:w-1/4 px-2 py-2'>Amount</th>
-                                    <th className='text-xl font-bold w-full md:w-1/4 px-2 py-2'>Action</th>
+                                    <th className='text-xl font-bold w-full md:w-1/4 mr-6    px-2 py-2'>Amount</th>
+                                    <th className='text-xl font-bold w-full md:w-1/4 px-2 py-2 '>Action</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -66,26 +66,32 @@ const Transaction = () => {
                                         <td className='w-full md:w-1/4 px-2 py-2'>{t.description}</td>
                                         <td className={`w-full md:w-1/4 px-2 py-2 ${t.amount < 0 ? 'text-red-500' : 'text-green-500'}`}>{t.amount}</td>
                                         <td className='w-full md:w-1/4 px-2 py-4'>
-                                            <button className='bg-green-500 px-2 py-2 text-white mr-1' onClick={() => handleEdit(t)}>Edit</button>
-                                            <button className='bg-red-500 px-2 py-2 text-white' onClick={() => handleDelete(t.id)}>Delete</button>
+                                            <div className=''>
+                                            <button className='bg-green-500 px-2  rounded-lg py-2 text-white mr-1' onClick={() => handleEdit(t)}>Edit</button>
+                                            <button className='bg-red-500 px-2 rounded-lg py-2 text-white' onClick={() => handleDelete(t.id)}>Delete</button>
+                                            </div>
                                         </td>
                                     </tr>
                                 ))}
                             </tbody>
-                        </table>
-                        <table className='w-full table-fixed flex flex-col items-between text-left'>
-                        <thead>
-                            <div className='mt-4 text-xl font-bold '>
-                                Income: <span className='text-green-500 '>{getIncome()}</span> 
+                         </table>
+                         <table className=' table-fixed flex flex-col items-between text-left'>
+                         <thead >
+                           
+                         <div className='justify-end flex flex-col bg-white p-4 rounded shadow-lg '>
+                                <div className='mt-4 text-xl font-bold mr-48 flex '>
+                                    Income: <span className='text-green-500 '>{getIncome()}</span>
                                 </div>
-                                <div className='mt-4 text-xl font-bold'>  
-                                Expense: <span className='text-red-500'>{getExpenses()}</span> 
+                                <div className='mt-4 text-xl font-bold mr-48 flex'>
+                                    Expense: <span className='text-red-500 '>{getExpenses()}</span>
                                 </div>
-                                <div className='mt-4 text-xl font-bold'>
-                                Total: <span className={getTotalAmount() < 0 ? 'text-red-500' : 'text-green-500'}>{getTotalAmount()}</span>
+                                <div className='mt-4 text-xl font-bold  mr-48'>
+                                    Total: <span className={getTotalAmount() < 0 ? 'text-red-500' : 'text-green-500'}>{getTotalAmount()}</span>
                                 </div>
-                                </thead>
-                        </table>
+                                </div>
+
+                         </thead>
+                         </table>
                         </div>
                         <div className='mt-16 mb-4 lg:w-1/2 xl:w-1/3 p-5 mx-auto rounded shadow-lg border'>
                             <h1 className='text-xl font-bold text-center mb-5'>Add Your Transaction</h1>
