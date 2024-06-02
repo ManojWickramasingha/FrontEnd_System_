a78e81f6921584a2ae44cbf1242f10369b0a2a34
Ashen06C <ashencharanga99@gmail.com>
2024-06-02 19:35:29 +0530

    Calender changes and start budget

--- src/pages/dashboard/user/Budget.jsx
+++ src/pages/dashboard/user/Budget.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React from "react";
 
 const Budget = () => {
-    // Your component logic here
+  // Your component logic here
 
-    return (
-        <div className="pageTemplate2">
-            {/* Your JSX here */}
-        </div>
-    );
+  return (
+    <div className="pageTemplate2">
+      <h1 style={{ fontSize: "30px", fontWeight: "bold" }}>Budget</h1>
+      
+    </div>
+  );
 };
 
 export default Budget;
