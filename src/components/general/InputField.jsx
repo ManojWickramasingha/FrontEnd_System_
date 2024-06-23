4ccb0c44f7cb6fd49928edcb3588552507272023
chinthakaSenarathna <chironprabath@gmail.com>
2024-06-23 15:38:07 +0530

    set Texterea to InputField component

--- src/components/general/InputField.jsx
+++ src/components/general/InputField.jsx
@@ -1,6 +1,6 @@
 import { Controller } from 'react-hook-form';
 
-const InputField = ({ control, label, inputName, inputType = 'text', error }) => {
+const InputField = ({ control, label, inputName, inputType = 'text', error, isTextarea = false }) => {
     const renderTopRow = () => {
         if (error) {
             return <span className='text-red-600 font-semibold'>{error}</span>;
@@ -14,18 +14,27 @@ const InputField = ({ control, label, inputName, inputType = 'text', error }) =>
     const dynamicClassName = error ? 'border-red-500 rounded-lg' : 'border-[#3eab73]';
 
     return (
-        <div className='px-4 my-2 w-9/12'>
+        <div className='px-12 my-2 w-full'>
             {renderTopRow()}
             <Controller
                 name={inputName}
                 control={control}
                 render={({ field }) => (
-                    <input
-                        {...field}
-                        autoComplete='off'
-                        type={inputType}
-                        className={dynamicClassName}
-                    />
+                    isTextarea ?
+                        <textarea
+                            {...field}
+                            autoComplete='off'
+                            rows={''}
+                            cols={''}
+                            className= {`w-full border-1 rounded-lg ${error ? 'border-red-500' : 'border-[#3eab73]'} focus:outline-none`}
+                        />
+                        :
+                        <input
+                            {...field}
+                            autoComplete='off'
+                            type={inputType}
+                            className={dynamicClassName}
+                        />
                 )}
             />
         </div>
